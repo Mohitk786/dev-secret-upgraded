7b25ec1ee5bc1b3abf64d591a9453d69619c6366
Mohitk786 <kumarmohit08004@gmail.com>
2025-04-27 16:01:23 +0530

    latest commit

--- apps/frontend/src/middleware.ts
+++ apps/frontend/src/middleware.ts
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get('dev_secret_vault_auth_token')?.value;
-
+  const token = await request.cookies.get('dev_secret_vault_auth_token')?.value;
+   console.log("getting ", token)
   if (!token) {
 
     return NextResponse.redirect(new URL('/login', request.url));
